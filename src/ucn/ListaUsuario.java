8321976a8f940a9a9ad87f2fa769c041f3cc89cc
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2018-08-18 22:35:38 -0300

    Commit

--- src/ucn/ListaUsuario.java
+++ src/ucn/ListaUsuario.java
@@ -1,5 +1,41 @@
 package ucn;
 
 public class ListaUsuario {
-
+	private NodoDobleUsuario head;
+	private int cant;
+	
+	public ListaUsuario(Usuario usuario) {
+		this.head = new NodoDobleUsuario(usuario);
+		this.cant++;
+		
+	}
+	
+	public void agregarusuario(Usuario usuario) {
+		NodoDobleUsuario aux = head;
+		NodoDobleUsuario nuevonodo = new NodoDobleUsuario(usuario);
+		while(aux.getSig()!=null) {
+			aux = aux.getSig();
+		}
+		aux.setSig(nuevonodo);
+		nuevonodo.setPreb(aux);
+		
+	}
+	
+	public Usuario buscarusuario(String email, String contraseña) {
+		if(head != null) {
+			NodoDobleUsuario aux = this.head;
+			for(int i=0; i<cant; i++) {
+				Usuario usuario = aux.getUsuario();
+				if(usuario.getEmail().equals(email) && usuario.getContraseña().equals(contraseña)) {
+					return usuario;
+				}
+				aux = aux.getSig();
+				
+			}
+			StdOut.println("\n XXXXXXX EL USUARIO NO EXISTE, INTENTALO DE NUEVO XXXXXXX\n");
+			return null;
+		}
+		StdOut.println("\nXXXXXX EL USUARIO NO FUE ENCONTRADO, PORFAVOR INTENTALO DE NUEVO XXXXXXX\n");
+		return null;
+	}
 }
--- src/ucn/Main.java
+++ src/ucn/Main.java
@@ -1,12 +1,36 @@
 package ucn;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
+		//EmiEmiImpl sistema = new EmiEmiImpl();
+		leer_usuarios();
 		menu();
 		
+	}
+	public static void leer_usuarios() throws IOException { 
+		ArchivoEntrada archivo_usuario = new ArchivoEntrada("C:\\Users\\aleja\\Documents\\Git\\Taller4\\Archivos\\Usuarios.txt");
+		while(!archivo_usuario.isEndFile()) {
+			Registro reg = archivo_usuario.getRegistro();
+			String email = reg.getString();
+			String contraseña = reg.getString();
+			String nombre = reg.getString();
+			String tipo = reg.getString();
+			if(tipo.equals("cliente")) {
+				String pais = reg.getString();
+				int puntos = reg.getInt();
+				Usuario cliente = new Cliente(email,contraseña,nombre,pais,puntos);
+				//Se agrega el cliente a la lista
+			}
+			else {
+				Usuario admin = new Usuario(email,contraseña,nombre);
+				//Se agrega el admin a la lista 
+			}
+			
+		}
 	}
 	
 	public static void menu() {
